ab66bf857fa0d518b8e3b68dedca69e105bfff0c
Uwimbabaziclemence <clemenceuwi22@gmail.com>
2025-04-13 18:02:26 +0200

    admin book issue management view added

--- resources/views/admin/issued-books/create.blade.php
+++ resources/views/admin/issued-books/create.blade.php
@@ -10,7 +10,6 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('issued-books.store') }}">
                         @csrf
-
                         <div class="row mb-3">
                             <label for="book_id" class="col-md-4 col-form-label text-md-end">Book</label>
                             <div class="col-md-6">
